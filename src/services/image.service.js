7f19c2e23b4b78efadd602986b2c5675a3b6bfed
DoLong <doduclong2208@gmail.com>
2021-03-23 10:13:59 +0700

    add search api

--- src/services/image.service.js
+++ src/services/image.service.js
@@ -1,16 +1,24 @@
 import axios from 'axios'
 const imageApi = axios.create({
-    baseURL: "https://api.pexels.com/v1/curated",
+    baseURL: "https://api.pexels.com/v1",
+    headers:{
+        'Authorization':'563492ad6f91700001000001892b4f17061142bd9351074140eadcd1'
+    }
 })
 export default {
     getImages(per_page=35) {
-        return imageApi.get('',{
+        return imageApi.get('/curated',{
             params: {
                 per_page
             },
-            headers:{
-                'Authorization':'563492ad6f91700001000001892b4f17061142bd9351074140eadcd1'
-            }
         });
+    },
+    searchImages(per_page=35,query){
+        return imageApi.get('/search',{
+            params: {
+                per_page,
+                query
+            },
+        })
     }
 }
