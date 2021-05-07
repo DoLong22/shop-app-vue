eae388f45cfcce116ae78f6f7dde23a0c6521145
DoLong <doduclong2208@gmail.com>
2021-05-07 23:51:49 +0700

    style filter option

--- src/components/Body/NavbarSide.vue
+++ src/components/Body/NavbarSide.vue
@@ -1,13 +1,15 @@
 <template>
-  <li class="nav-item">
-    <select class="" id="filterByPrice" @change="filterByPrice">
-      <option selected>All price</option>
-      <option value="1">From 1.000 to 15.000</option>
-      <option value="2">From 15.000 to 30.000</option>
-      <option value="3">From 30.000 to 45.000</option>
-      <option value="4">Greater 45.000</option>
-    </select>
-  </li>
+  <ul class="list-category">
+    <li class="nav-item">
+      <select class="" id="filterByPrice" @change="filterByPrice">
+        <option selected>All price</option>
+        <option value="1">From 1.000 to 15.000</option>
+        <option value="2">From 15.000 to 30.000</option>
+        <option value="3">From 30.000 to 45.000</option>
+        <option value="4">Greater 45.000</option>
+      </select>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -25,6 +27,6 @@ export default {
 };
 </script>
 
-<style lang='scss'>
+<style lang='scss' scoped>
 @import "@/resources/styles/navbar-side.style.scss";
 </style>
